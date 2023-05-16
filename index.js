d03866f3bad81232e8ebb520d1b1d503c5a9bf3a
Dasari Umamahesh <um.dasari24@gmail.com>
2023-05-16 19:43:25 +0530

    2 dn nd push

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+console.log("_________________Here_______________________")
 
-app.get('/test', (req,res)=>res.send("tets route working"))
+app.get('/test', (req,res)=>{
+    console.log("_________________Here")
+    res.send("tets route working")
+})
 
 app.listen(3000,'0.0.0.0',()=>{
     console.log("running on port 3000")
